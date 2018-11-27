8691ab8eb412f89cf0648275c9fa0d7a73f61922
Kaloyan K. Tsvetkov <kaloyan@kaloyan.info>
2018-11-27 14:31:29 +0200

    Adding Fuko\Masked\Tests\ProtectHideInputsTest

---
+++ tests/ProtectHideInputsTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Fuko\Masked\Tests;
+
+use Fuko\Masked\Protect;
+use PHPUnit\Framework\TestCase;
+
+class ProtectHideInputsTest extends TestCase
+{
+	function tearDown()
+	{
+		Protect::clearInputs();
+	}
+
+	/**
+	* @dataProvider provider_hideInput
+	* @covers Fuko\Masked\Protect::hideInput
+	* @covers Fuko\Masked\Protect::_validateInput
+	*/
+	function test_hideInput($name, $type)
+	{
+		$this->assertTrue(
+			Protect::hideInput($name, $type)
+		);
+	}
+
+	function provider_hideInput()
+	{
+		return array(
+			array('clandestine', INPUT_POST),
+			array('incognito', 333),
+			array('OLDPWD', INPUT_ENV),
+			array('USER', INPUT_SERVER),
+		);
+	}
+
+	/**
+	* @dataProvider provider_hideInput_bad
+	* @covers Fuko\Masked\Protect::hideInput
+	* @covers Fuko\Masked\Protect::_validateInput
+	* @expectedException PHPUnit_Framework_Error_Warning
+	*/
+	function test_hideInput_bad($name, $type)
+	{
+		Protect::hideInput($input, $type);
+	}
+
+	function provider_hideInput_bad()
+	{
+		return array(
+
+			array('', INPUT_SESSION),
+			array(array(), INPUT_REQUEST),
+			array(null, INPUT_COOKIE),
+			array($this, INPUT_POST),
+
+			array('cipher', null),
+			array('cipher', ''),
+			array('cipher', array()),
+			array('cipher', $this),
+		);
+	}
+
+	/**
+	* @dataProvider provider_hideInputs_bad
+	* @covers Fuko\Masked\Protect::hideInputs
+	* @covers Fuko\Masked\Protect::_validateInput
+	* @expectedException PHPUnit_Framework_Error_Warning
+	*/
+	function test_hideInputs_bad($inputs)
+	{
+		Protect::hideInputs( $inputs );
+	}
+
+	function provider_hideInputs_bad()
+	{
+		return array(
+			array( array( 44 => null ) ),
+			array( array( null ) ),
+			array( array( '' ) ),
+
+			array( array( 33 => (object) $_SERVER ) ),
+			array( array( 22 => fopen(__FILE__, 'r') ) ),
+			array( array( 22 => opendir(__DIR__) ) ),
+
+			array( array( 33 => array( (object) $_SERVER ) ) ),
+			array( array( 22 => array( fopen(__FILE__, 'r') ) ) ),
+			array( array( 22 => array( opendir(__DIR__) ) ) ),
+		);
+	}
+}
